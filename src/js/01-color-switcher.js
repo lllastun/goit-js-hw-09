dc01f84b0ee922998859990dd392a756983597ac
lllastun <lllastun@gmail.com>
2023-04-27 12:55:27 +0300

    01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -7,7 +7,8 @@ const buttonStyle = 'font-size: 22px; padding: 10px 16px; margin: 0px 5px';
 const changeBgColorIsActive = false;
 const buttonWrapper = document.createElement('div');
 buttonWrapper.className = 'buttonWapper';
-buttonWrapper.style.cssText = ('position: fixed; top: 50%; left: 50%; transform: translate(-50%,-50%)');
+buttonWrapper.style.cssText =
+  'position: fixed; top: 50%; left: 50%; transform: translate(-50%,-50%)';
 buttonStartElement.style.cssText = buttonStyle;
 buttonStopElement.style.cssText = buttonStyle;
 buttonStopElement.disabled = true;
@@ -16,23 +17,32 @@ buttonStopElement.textContent = buttonStopElement.textContent.toUpperCase();
 
 buttonWrapper.append(buttonStartElement);
 buttonWrapper.append(buttonStopElement);
-bodyElement.insertBefore (buttonWrapper, scriptElement);
+bodyElement.insertBefore(buttonWrapper, scriptElement);
+let timerId;
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
 function changeBodyBgColor() {
   bodyElement.style.backgroundColor = getRandomHexColor();
 }
 
-const timerId = setTimeout(() => {
-  
-}, timeout);
-
-const onClickStart = e =>  {
+function onClickStart() {
+  buttonStartElement.disabled = true;
+  buttonStopElement.disabled = false;
   changeBodyBgColor();
+  timerId = setInterval(changeBodyBgColor, 1000);
+}
+
+function onClickStop() {
+  buttonStartElement.disabled = false;
+  buttonStopElement.disabled = true;
+  clearInterval(timerId);
 }
 buttonWrapper.insertBefore(buttonStartElement, null);
 buttonWrapper.insertBefore(buttonStopElement, null);
-buttonStartElement.addEventListener("click", onClickStart);
+buttonStartElement.addEventListener('click', onClickStart);
+buttonStopElement.addEventListener('click', onClickStop);
